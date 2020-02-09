230a2086af9fecf8fd29828a1b811e580bd5a539
Kiss Bence <kissbence2000@gmail.com>
2020-02-09 15:53:15 +0100

    Kilépés módosítása, Kijelentezés funkció a HomePage-en

--- app/src/main/java/com/example/banki_alkalmazas_szakdolgozat_2019_2020/HomePage.java
+++ app/src/main/java/com/example/banki_alkalmazas_szakdolgozat_2019_2020/HomePage.java
@@ -1,20 +1,27 @@
 package com.example.banki_alkalmazas_szakdolgozat_2019_2020;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.net.Inet4Address;
+
 public class HomePage extends AppCompatActivity {
     private TextView tvWelc;
-    private Button btnKartya,btnTranzakcio,btnKilep;
-    private DatabaseReference databaseReference;
+    private Button btnKartya,btnTranzakcio,btnKilep,btnKijelentkezes;
+    private FirebaseAuth firebaseAuth;
+    private AlertDialog alertDialog;
+    private AlertDialog.Builder alertDialogBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +32,23 @@ public class HomePage extends AppCompatActivity {
         btnKilep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-                System.exit(0);
+                alertDialogBuilder = new AlertDialog.Builder(HomePage.this);
+                alertDialogBuilder.setMessage("Biztos ki akarsz lépni az alkalmazásból?");
+                alertDialogBuilder.setPositiveButton("Nem", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alertDialogBuilder.setNegativeButton("Igen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                alertDialogBuilder.setCancelable(false);
+                alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
             }
         });
 
@@ -47,6 +69,16 @@ public class HomePage extends AppCompatActivity {
                 finish();
             }
         });
+
+        btnKijelentkezes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                Intent intent=new Intent(HomePage.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     public void init(){
@@ -54,5 +86,8 @@ public class HomePage extends AppCompatActivity {
         btnKartya=findViewById(R.id.btnCard);
         btnTranzakcio=findViewById(R.id.btnTransaction);
         btnKilep=findViewById(R.id.btnExit);
+        btnKijelentkezes=findViewById(R.id.btnSignOut);
+
+        firebaseAuth=FirebaseAuth.getInstance();
     }
 }
